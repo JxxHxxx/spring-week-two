ceb819d77eaa2303905e81e070e676cf8485b63d
JxxHxxx <leesin5498@naver.com>
2023-01-02 01:13:23 +0900

    TEST #2 MemberService 테스트
    회원가입 unit 테스트
    정상
    중복 username
    필드 유효성 검증

--- src/main/java/com/sparta/springweektwo/member/dto/LoginDto.java
+++ src/main/java/com/sparta/springweektwo/member/dto/LoginDto.java
@@ -1,8 +1,10 @@
 package com.sparta.springweektwo.member.dto;
 
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
+@Setter // 테스트 용
 public class LoginDto {
     private String username;
     private String password;
--- src/main/java/com/sparta/springweektwo/member/dto/SignUpRequestDto.java
+++ src/main/java/com/sparta/springweektwo/member/dto/SignUpRequestDto.java
@@ -2,11 +2,13 @@ package com.sparta.springweektwo.member.dto;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 @Getter
+@Setter // 테스트 용
 @NoArgsConstructor
 public class SignUpRequestDto {
 
