762484b182fcd3b97782b798a772494ca46d981c
JxxHxxx <leesin5498@naver.com>
2023-01-01 17:13:45 +0900

    FEAT #3 게시글 작성 요청 Dto 요구 사항에 맞게 변경

--- src/main/java/com/sparta/springweektwo/bulletinboard/dto/BulletinBoardForm.java
+++ src/main/java/com/sparta/springweektwo/bulletinboard/dto/BulletinBoardForm.java
@@ -6,12 +6,10 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 public class BulletinBoardForm {
-    private String password;
     private String title;
     private String body;
 
-    public BulletinBoardForm(String password, String title, String body) {
-        this.password = password;
+    public BulletinBoardForm(String title, String body) {
         this.title = title;
         this.body = body;
     }
