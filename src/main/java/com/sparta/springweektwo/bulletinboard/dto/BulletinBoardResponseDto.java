c6b847f81116b55cc555c53f54d54febbad4e1ad
JxxHxxx <leesin5498@naver.com>
2023-01-01 23:48:44 +0900

    FEAT #3 Dto 논리 삭제 여부 필드 추가

--- src/main/java/com/sparta/springweektwo/bulletinboard/dto/BulletinBoardResponseDto.java
+++ src/main/java/com/sparta/springweektwo/bulletinboard/dto/BulletinBoardResponseDto.java
@@ -16,13 +16,15 @@ public class BulletinBoardResponseDto {
     private String username;
     private LocalDateTime createAt;
     private LocalDateTime modifiedAt;
+    private Boolean isDeleted;
 
     public BulletinBoardResponseDto(BulletinBoard board) {
         this.id = board.getId();
         this.title = board.getTitle();
-        this.username = board.getMember().getUsername();
+        this.username = board.getUsername();
         this.body = board.getBody();
         this.createAt = board.getCreateAt();
         this.modifiedAt = board.getModifiedAt();
+        this.isDeleted = board.getIsDeleted();
     }
 }
