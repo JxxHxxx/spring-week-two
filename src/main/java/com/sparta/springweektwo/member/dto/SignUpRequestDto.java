2518445a21b04a92ced80047d19951214b38f454
JxxHxxx <leesin5498@naver.com>
2023-01-04 03:51:52 +0900

    FEAT #4 패스워드 요구사항 추가 특수문자 포함

--- src/main/java/com/sparta/springweektwo/member/dto/SignUpRequestDto.java
+++ src/main/java/com/sparta/springweektwo/member/dto/SignUpRequestDto.java
@@ -1,15 +1,12 @@
 package com.sparta.springweektwo.member.dto;
 
+import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
 import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 @Getter
-@Setter // 테스트 용
-@NoArgsConstructor
+@Builder
 public class SignUpRequestDto {
 
     @Size(min = 4, max = 10)
@@ -17,6 +14,11 @@ public class SignUpRequestDto {
     private String username;
 
     @Size(min = 8, max = 15)
-    @Pattern(regexp = "[0-9a-zA-Z]+")
+    @Pattern(regexp = "[0-9a-zA-Z!@#$%^&*(),.?\":{}|<>]+")
     private String password;
+
+    public SignUpRequestDto(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
 }
